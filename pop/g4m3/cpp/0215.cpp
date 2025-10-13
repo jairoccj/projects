991c2ec59c8e67a52e7cc6be0b2e1b1413856f45
Jairo <jairoccostajr@gmail.com>
2025-10-13 00:02:43 -0300

    feat: Adiciona P0215 do G4M3

--- pop/g4m3/cpp/0215.cpp
+++ pop/g4m3/cpp/0215.cpp
@@ -1,22 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long long
+#define vll vector<long long>
 
-int main(){
-    int n, seq; cin >> n >> seq;
+void solve(){
+    ll n, m; cin >> n >> m;
+    vll v(n);
+    for(ll &x : v) cin >> x;
 
-    vector<int> numeros(n,0);
-    for (int &a : numeros) cin >> a;
+    ll l=0, r=1;
 
-    int maior = 0;
+    ll sol = 0;
 
-    int esquerda = 0, soma = 0;
+    ll calc = v[0];
 
-    int valor = 0;
-
-    for(int direita=0; direita<n; direita++){
-        
+    while(r<n){
+        while(r-l+1 > m){
+            calc = calc ^ v[l];
+            l++;
+        }
+        calc = calc ^ v[r];
+        if(r-l+1 == m){
+            sol = max(sol, calc);
+        }
+        r++;
     }
 
-
-
+    cout << sol << '\n';
 }
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+}
+
+// 6 3
+// 10 7 6 4 10 10
