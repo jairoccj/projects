7a2cf9e1efcda16fe7e9b839cd32adaf8c524577
Jairo <jairoccostajr@gmail.com>
2025-10-16 21:07:43 -0300

    feat: Adiciona 50A

---
+++ codeforces/cpp/50a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int num1, num2; cin >> num1 >> num2;
+
+    int area = num1*num2;
+    cout << area/2 << '\n';
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    solve();
+}
